226395c42e3656511e8e4b62e0e5865f2000e9b6
mahibashardeveloper <mahibashar2023@gmail.com>
2024-03-09 11:44:10 +0600

    update layout and avatar issues

--- resources/js/admin/layout/layout.vue
+++ resources/js/admin/layout/layout.vue
@@ -3,54 +3,77 @@
     <div class="panel">
 
         <!-- sidebar -->
-        <div class="sidebar">
+        <div class="sidebar" :class="{active: sidebarToggle}">
+
+            <!-- sidebar header -->
             <div class="sidebar-header">
-                <a href="javascript:void(0)" class="sidebar-header-link">
+                <router-link :to="{name: 'dashboard'}" class="sidebar-header-link" @click="sideBarToggle()">
                     <span class="res-link fw-bold">H</span>
                     <span class="text-link">Hospital</span>
-                </a>
-                <button type="button" class="btn btn-icon sidebar-close border-0">
+                </router-link>
+                <button type="button" class="btn btn-icon sidebar-close border-0" @click="sideBarToggle()">
                     <i class="bi bi-x-lg"></i>
                 </button>
             </div>
+
+            <!-- sidebar body -->
             <div class="sidebar-body">
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                <router-link :to="{name: 'dashboard'}" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-speedometer"></i>
-                    <span class="ms-2 text-link"> Dashboard </span>
-                </a>
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                    <span class="ms-2 text-link">
+                        Dashboard
+                    </span>
+                </router-link>
+                <a href="javascript:void(0)" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-sliders"></i>
-                    <span class="ms-2 text-link"> Slider management </span>
+                    <span class="ms-2 text-link">
+                        Slider management
+                    </span>
                 </a>
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                <a href="javascript:void(0)" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-boxes"></i>
-                    <span class="ms-2 text-link"> Box management </span>
+                    <span class="ms-2 text-link">
+                        Box management
+                    </span>
                 </a>
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                <a href="javascript:void(0)" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-heart-pulse"></i>
-                    <span class="ms-2 text-link"> Doctors </span>
+                    <span class="ms-2 text-link">
+                        Doctors
+                    </span>
                 </a>
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                <a href="javascript:void(0)" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-hearts"></i>
-                    <span class="ms-2 text-link"> Feedbacks </span>
+                    <span class="ms-2 text-link">
+                        Feedbacks
+                    </span>
                 </a>
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                <a href="javascript:void(0)" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-clipboard2-heart"></i>
-                    <span class="ms-2 text-link"> Services </span>
+                    <span class="ms-2 text-link">
+                        Services
+                    </span>
                 </a>
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                <a href="javascript:void(0)" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-postage"></i>
-                    <span class="ms-2 text-link"> Blogs </span>
+                    <span class="ms-2 text-link">
+                        Blogs
+                    </span>
                 </a>
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                <a href="javascript:void(0)" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-calendar-event"></i>
-                    <span class="ms-2 text-link"> Events </span>
+                    <span class="ms-2 text-link">
+                        Events
+                    </span>
                 </a>
-                <a href="javascript:void(0)" class="sidebar-body-link">
+                <a href="javascript:void(0)" class="sidebar-body-link" @click="sideBarToggle()">
                     <i class="bi bi-bookmark"></i>
-                    <span class="ms-2 text-link"> Departments </span>
+                    <span class="ms-2 text-link">
+                        Departments
+                    </span>
                 </a>
             </div>
+
         </div>
 
         <div class="content">
@@ -59,21 +82,28 @@
             <div class="content-header">
 
                 <div class="container-fluid">
+
                     <div class="content-header-inner">
-                        <button type="button" class="btn btn-icon sidebar-toggle border-0">
+
+                        <!-- sidebar toggle -->
+                        <button type="button" class="btn btn-icon border-0" @click="sideBarToggle">
                             <i class="bi bi-justify-left"></i>
                         </button>
+
+                        <!-- group of buttons -->
                         <div class="d-flex justify-content-end align-items-center">
                             <button type="button" class="btn btn-icon border-0 ms-2">
                                 <i class="bi bi-envelope"></i>
                             </button>
+
                             <button type="button" class="btn btn-icon border-0 ms-2">
                                 <i class="bi bi-bell"></i>
                             </button>
 
+                            <!-- dropdown-menu -->
                             <div class="dropdown">
                                 <button type="button" class="btn border-0 btn-user" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img :src="`assets/avatar/avatar.png`" class="user-image rounded-circle" alt="avatar">
+                                    <img :src="`/images/avatar/avatar.png`" class="user-image rounded-circle" alt="avatar">
                                 </button>
                                 <ul class="dropdown-menu dropdown-menu-end p-0 mt-3 me-2 overflow-hidden shadow border-0">
                                     <li>
@@ -93,8 +123,11 @@
                                     </li>
                                 </ul>
                             </div>
+
                         </div>
+
                     </div>
+
                 </div>
 
             </div>
@@ -102,7 +135,11 @@
             <!-- content body -->
             <div class="content-body p-3">
 
-                <router-view/>
+                <div class="container-fluid">
+
+                    <router-view/>
+
+                </div>
 
             </div>
 
@@ -120,7 +157,7 @@ export default {
 
         return{
 
-
+            sidebarToggle: false,
 
         }
 
@@ -134,7 +171,9 @@ export default {
 
     methods: {
 
-
+        sideBarToggle() {
+            this.sidebarToggle = !this.sidebarToggle;
+        }
 
     }
 
--- resources/js/admin/pages/dashboard.vue
+++ resources/js/admin/pages/dashboard.vue
@@ -1,5 +1,152 @@
 <template>
 
+    <!-- breadcrumb -->
+    <div class="d-flex align-items-center justify-content-start">
+        <div class="mb-2 text-light-gray breadcrumb-title">
+            Dashboard
+        </div>
+    </div>
+
+    <div class="row">
+
+        <!-- slider count -->
+        <div class="mb-3 p-2 col-12 col-sm-6 col-xl-4 col-xxl-3">
+            <a href="javascript:void(0)" class="card card-hover border-0 text-center text-decoration-none">
+                <div class="card-body py-4 px-2">
+                    <div class="mb-3 fs-4 d-block">
+                        <i class="bi bi-sliders"></i>
+                    </div>
+                    <div class="mb-3 fs-6 effect">
+                        Summary slider management
+                    </div>
+                    <div class="text-light-gray fs-4 d-block">
+                        2250
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- box count -->
+        <div class="p-2 col-12 col-sm-6 col-xl-4 col-xxl-3">
+            <a href="javascript:void(0)" class="card card-hover border-0 text-center text-decoration-none">
+                <div class="card-body py-4 px-2">
+                    <div class="mb-3 fs-4 d-block">
+                        <i class="bi bi-boxes"></i>
+                    </div>
+                    <div class="mb-3 fs-6 effect">
+                        Summary Box management
+                    </div>
+                    <div class="text-light-gray fs-4 d-block">
+                        2250
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- doctor count -->
+        <div class="p-2 col-12 col-sm-6 col-xl-4 col-xxl-3">
+            <a href="javascript:void(0)" class="card card-hover border-0 text-center text-decoration-none">
+                <div class="card-body py-4 px-2">
+                    <div class="mb-3 fs-4 d-block">
+                        <i class="bi bi-heart-pulse"></i>
+                    </div>
+                    <div class="mb-3 fs-6 effect">
+                        Summary Doctors
+                    </div>
+                    <div class="text-light-gray fs-4 d-block">
+                        2250
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- feedback count -->
+        <div class="p-2 col-12 col-sm-6 col-xl-4 col-xxl-3">
+            <a href="javascript:void(0)" class="card card-hover border-0 text-center text-decoration-none">
+                <div class="card-body py-4 px-2">
+                    <div class="mb-3 fs-4 d-block">
+                        <i class="bi bi-hearts"></i>
+                    </div>
+                    <div class="mb-3 fs-6 effect">
+                        Summary Feedbacks
+                    </div>
+                    <div class="text-light-gray fs-4 d-block">
+                        2250
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- service count -->
+        <div class="p-2 col-12 col-sm-6 col-xl-4 col-xxl-3">
+            <a href="javascript:void(0)" class="card card-hover border-0 text-center text-decoration-none">
+                <div class="card-body py-4 px-2">
+                    <div class="mb-3 fs-4 d-block">
+                        <i class="bi bi-clipboard2-heart"></i>
+                    </div>
+                    <div class="mb-3 fs-6 effect">
+                        Summary Services
+                    </div>
+                    <div class="text-light-gray fs-4 d-block">
+                        2250
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- blog count -->
+        <div class="p-2 col-12 col-sm-6 col-xl-4 col-xxl-3">
+            <a href="javascript:void(0)" class="card card-hover border-0 text-center text-decoration-none">
+                <div class="card-body py-4 px-2">
+                    <div class="mb-3 fs-4 d-block">
+                        <i class="bi bi-postage"></i>
+                    </div>
+                    <div class="mb-3 fs-6 effect">
+                        Summary Blogs
+                    </div>
+                    <div class="text-light-gray fs-4 d-block">
+                        2250
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- event count -->
+        <div class="p-2 col-12 col-sm-6 col-xl-4 col-xxl-3">
+            <a href="javascript:void(0)" class="card card-hover border-0 text-center text-decoration-none">
+                <div class="card-body py-4 px-2">
+                    <div class="mb-3 fs-4 d-block">
+                        <i class="bi bi-calendar-event"></i>
+                    </div>
+                    <div class="mb-3 fs-6 effect">
+                        Summary Events
+                    </div>
+                    <div class="text-light-gray fs-4 d-block">
+                        2250
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- department count -->
+        <div class="p-2 col-12 col-sm-6 col-xl-4 col-xxl-3">
+            <a href="javascript:void(0)" class="card card-hover border-0 text-center text-decoration-none">
+                <div class="card-body py-4 px-2">
+                    <div class="mb-3 fs-4 d-block">
+                        <i class="bi bi-bookmark"></i>
+                    </div>
+                    <div class="mb-3 fs-6 effect">
+                        Summary Departments
+                    </div>
+                    <div class="text-light-gray fs-4 d-block">
+                        2250
+                    </div>
+                </div>
+            </a>
+        </div>
+
+    </div>
+
 </template>
 
 <script>
