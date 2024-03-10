f42c54988e3329b969b3258a1b873d2aadd5cb55
mahibashardeveloper <mahibashar2023@gmail.com>
2024-03-10 10:05:58 +0600

    update

--- resources/js/admin/pages/box/box.vue
+++ resources/js/admin/pages/box/box.vue
@@ -26,59 +26,6 @@
 
     </div>
 
-    <div class="card border-0">
-        <div class="card-body">
-
-            <!-- search and new -->
-            <div class="row px-2">
-                <div class="col-md-4 pt-1 pb-3 px-2">
-
-                    <!-- search -->
-                    <div class="position-relative">
-                        <input type="text" name="keyword" class="form-control shadow-none ps-5 rounded-1" required placeholder="Search Here" autocomplete="new-search">
-                        <div class="position-absolute top-50 start-0 translate-middle-y ps-3">
-                            <i class="bi bi-search text-light-gray"></i>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-md-8 pt-1 pb-3 px-2">
-
-                    <!-- new -->
-                    <div class="w-100 d-flex justify-content-end">
-                        <router-link :to="{name: 'boxManage'}" class="btn btn-primary px-5 rounded-1">
-                            New
-                        </router-link>
-                    </div>
-
-                </div>
-            </div>
-
-            <!-- list data -->
-            <div class="card-list">
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Box Icon</th>
-                                <th>Box Title</th>
-                                <th>Box Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="each in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]">
-                                <td>Box Icon {{each}}</td>
-                                <td>Box Title {{each}}</td>
-                                <td>Box Description {{each}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
 </template>
 
 <script>
--- resources/js/admin/pages/slider/slider.vue
+++ resources/js/admin/pages/slider/slider.vue
@@ -25,54 +25,6 @@
 
     </div>
 
-    <div class="card border-0">
-        <div class="card-body">
-
-            <!-- search and new -->
-            <div class="row px-2">
-                <div class="col-md-4 pt-1 pb-3 px-2">
-
-                    <!-- search -->
-                    <div class="position-relative">
-                        <input type="text" name="keyword" class="form-control shadow-none ps-5 rounded-1" required placeholder="Search Here" autocomplete="new-search">
-                        <div class="position-absolute top-50 start-0 translate-middle-y ps-3">
-                            <i class="bi bi-search text-light-gray"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-8 pt-1 pb-3 px-2">
-
-                    <!-- new -->
-                    <div class="w-100 d-flex justify-content-end">
-                        <router-link :to="{name: 'sliderManage'}" class="btn btn-primary px-5 rounded-1">
-                            New
-                        </router-link>
-                    </div>
-
-                </div>
-            </div>
-
-            <!-- list data -->
-            <div class="card-list">
-
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
-
-                    <router-link :to="{name: 'sliderManage'}" class="p-3 text-decoration-none text-light-gray">
-                        <span class="border p-0 d-block">
-                            <span class="border-bottom w-100 d-block" style="height: 250px;"></span>
-                            <span class="p-3 pb-0 d-block">First Line</span>
-                            <span class="p-3 pb-0 d-block">Second Line</span>
-                            <span class="p-3 d-block">Third Line</span>
-                        </span>
-                    </router-link>
-
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-
 </template>
 
 <script>
