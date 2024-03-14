1c7b8641e911cd5e5bb5c0b6d314bb8fbcdc46dc
mahibashardeveloper <mahibashar2023@gmail.com>
2024-03-15 05:49:52 +0600

    update ui appointment

--- resources/js/front/pages/appointment.vue
+++ resources/js/front/pages/appointment.vue
@@ -73,85 +73,7 @@
             </div>
             <div class="col-lg-4">
 
-                <div class="py-4">
-
-                    <div class="position-relative">
-                        <input type="text" name="keyword" class="form-control ps-5" placeholder="Search here" required autocomplete="new-keyword">
-                        <div class="position-absolute top-50 start-0 translate-middle-y ps-3">
-                            <i class="bi bi-search text-light-gray"></i>
-                        </div>
-                    </div>
-
-                    <div class="mt-3 fs-4 text-light-gray">
-                        Blog categories
-                    </div>
-
-                    <div class="mt-3">
-                        <a href="javascript:void(0)" class="link-icon-hover py-2 mb-2">
-                            <span class="d-flex align-items-center">
-                                <i class="bi bi-folder-fill me-2"></i>
-                                Blogs
-                            </span>
-                            <span class="badge bg-badge">( 20 )</span>
-                        </a>
-                        <a href="javascript:void(0)" class="link-icon-hover py-2 mb-2">
-                            <span class="d-flex align-items-center">
-                                <i class="bi bi-folder-fill me-2"></i>
-                                Counselling
-                            </span>
-                            <span class="badge bg-badge">( 15 )</span>
-                        </a>
-                        <a href="javascript:void(0)" class="link-icon-hover py-2 mb-2">
-                            <span class="d-flex align-items-center">
-                                <i class="bi bi-folder-fill me-2"></i>
-                                Health
-                            </span>
-                            <span class="badge bg-badge">( 10 )</span>
-                        </a>
-                        <a href="javascript:void(0)" class="link-icon-hover py-2 mb-2">
-                            <span class="d-flex align-items-center">
-                                <i class="bi bi-folder-fill me-2"></i>
-                                Kids
-                            </span>
-                            <span class="badge bg-badge">( 18 )</span>
-                        </a>
-                    </div>
-
-                    <div class="mt-3 fs-4 text-light-gray">
-                        Blog Tags
-                    </div>
-
-                    <div class="mt-3">
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Beach
-                        </button>
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Health
-                        </button>
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Holiday
-                        </button>
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Information
-                        </button>
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Kids
-                        </button>
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Music
-                        </button>
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Nature
-                        </button>
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Random Stuff
-                        </button>
-                        <button type="button" class="btn btn-outline-theme px-3 py-1 m-1">
-                            Rock
-                        </button>
-                    </div>
-
-                </div>
+                <SearchBlogTag/>
 
             </div>
         </div>
@@ -160,8 +82,13 @@
 </template>
 
 <script>
+import SearchBlogTag from "../component/searchBlogTag.vue";
 
 export default {
+
+    components: {
+        SearchBlogTag
+    },
 
     data(){
 
